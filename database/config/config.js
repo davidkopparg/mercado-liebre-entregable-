c4410bb5cefdbbae2ddd35996182c1b7aa423d7b
davidkopparg <davidkopparg@gmail.com>
2021-02-14 20:03:23 -0300

    instalado sequalize y dotenv

---
+++ database/config/config.js
@@ -0,0 +1,28 @@
+require("dotenv").config()
+
+
+module.exports =
+{
+  "development": {
+    "username": process.env.USERNAME_DB,
+    "password": process.env.PASSWORD_DB,
+    "database": process.env.NAME_DB,
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorAliases": false
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
