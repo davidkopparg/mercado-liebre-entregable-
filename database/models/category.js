41c14bafadd25b5b39c7c043514223dfb84cb036
davidkopparg <davidkopparg@gmail.com>
2021-02-20 07:41:02 -0300

    creado modelo category con sus relaciones

---
+++ database/models/category.js
@@ -0,0 +1,39 @@
+module.exports =  function(sequelize, dataTypes){
+
+    let alias = "Categories";
+
+    let cols={
+
+        id:{
+            type : dataTypes.INTEGER ,  
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name:{
+            type : dataTypes.STRING(100),
+        },
+
+        }
+        let config = {
+            tableName: "categories",
+            timestamps: true,
+            createdAt: "created_at",
+            updatedAt: "updated_at",
+            deletedAt: "deleted_at", 
+            paranoid: true
+        }
+
+    let Categories = sequelize.define(alias, cols, config);
+
+    Categories.associate = function(models){             
+        Categories.hasMany(models.Products, {
+      
+            as: "categories", 
+            foreignKey: "category_id"
+        })
+    }
+
+    return Categories;
+
+
+}
