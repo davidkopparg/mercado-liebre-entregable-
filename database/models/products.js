8b32e411af9085c6c13c230bce8e50bb06b59a8d
davidkopparg <davidkopparg@gmail.com>
2021-02-20 07:31:51 -0300

    creado modelo producto con sus dos relaciones

---
+++ database/models/products.js
@@ -0,0 +1,89 @@
+module.exports =  function(sequelize, dataTypes){
+
+    let alias = "Products"; 
+    
+    
+    let cols={
+
+        id:{
+            type : dataTypes.INTEGER ,  
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        title:{
+            type : dataTypes.STRING(100),
+            allowNull: false
+        },
+
+        description:{ 
+            type : dataTypes.STRING(255),
+            allowNull: false
+        },
+
+        photo:{ 
+            type : dataTypes.STRING(100),
+            allowNull: false
+        },
+
+        price:{ 
+            type : dataTypes.FLOAT(10,2)
+        },
+
+
+        stock:{ 
+            type : dataTypes.INTEGER,
+            allowNull: false
+        },
+
+        brand_id:{ 
+            type : dataTypes.INTEGER,
+           
+        },
+        
+        category_id:{ 
+            type : dataTypes.INTEGER,
+            allowNull: false
+        },
+        
+               
+
+        }
+        let config = {
+            tableName: "products",
+            timestamps: true,
+            createdAt: "created_at",
+           updatedAt: "updated_at",
+           deletedAt: "deleted_at",
+            paranoid: true
+    }
+
+    let Products = sequelize.define(alias, cols, config);
+
+
+    Products.associate = function(models){
+           
+         
+         
+           
+            
+        Products.belongsTo(models.Categorys, {
+        
+            as: "category",
+            foreignKey: "category_id"
+        }),
+
+            
+        Products.belongsTo(models.Brands, {
+        
+            as: "brands",
+            foreignKey: "brand_id"
+        })
+
+          
+
+    }    
+    
+    return Products;
+
+}
