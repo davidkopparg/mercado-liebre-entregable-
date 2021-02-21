e06832d99d1c0b3fe6054f1d9d099894ada57a7f
davidkopparg <davidkopparg@gmail.com>
2021-02-21 11:35:34 -0300

    creado metodo destroy con soft delete

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,5 +1,7 @@
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-const { promiseImpl } = require("ejs");
+const {
+	promiseImpl
+} = require("ejs");
 const db = require("../../database/models/index")
 
 
@@ -7,13 +9,15 @@ const controller = {
 	// Root - Show all products
 	index: (req, res) => {
 		db.Products.findAll()
-        .then(function(products){
-		
-			return res.render('products', { products:products});
-        })
-		.catch(function(err) {
-			console.log(err)
-		  })  
+			.then(function (products) {
+
+				return res.render('products', {
+					products: products
+				});
+			})
+			.catch(function (err) {
+				console.log(err)
+			})
 
 	},
 
@@ -21,92 +25,115 @@ const controller = {
 	detail: (req, res) => {
 
 		db.Products.findByPk(req.params.id, {
-			
-		  })
-		  .then(function(product){
-		   
-	return res.render('detail', { product:product});
-	  
-		  }).catch(function(err) {
-		console.log(err)
-		  })  
-			
-		
+
+			})
+			.then(function (product) {
+
+				return res.render('detail', {
+					product: product
+				});
+
+			}).catch(function (err) {
+				console.log(err)
+			})
+
+
 	},
 
 	// Create - Form to create
 	create: (req, res) => {
 		let marcas = db.Brands.findAll();
-		let categorias= db.Categories.findAll();
+		let categorias = db.Categories.findAll();
 
-		Promise.all([marcas,categorias])
-		.then(function([marcas,categorias]){
-				res.render("product-create-form",{marcas,categorias})
+		Promise.all([marcas, categorias])
+			.then(function ([marcas, categorias]) {
+				res.render("product-create-form", {
+					marcas,
+					categorias
+				})
 
-		})
-	  .catch(function(err){
-		  console.log(err)
-	  })
+			})
+			.catch(function (err) {
+				console.log(err)
+			})
 	},
-	
+
 	// Create -  Method to store
 	store: (req, res) => {
 		db.Products.create({
-			title: req.body.title,
-			description: req.body.description,
-			photo: "/images/products/"+ req.files[0].filename,
-			price: req.body.price,
-			stock: req.body.stock,
-			brand_id: req.body.brand,   
-			category_id: req.body.category,
-			
-		  }).then(data=>res.redirect("/products/"+data.id))
-	
-		  .catch(error=>console.log(error))
-	
-	 
+				title: req.body.title,
+				description: req.body.description,
+				photo: "/images/products/" + req.files[0].filename,
+				price: req.body.price,
+				stock: req.body.stock,
+				brand_id: req.body.brand,
+				category_id: req.body.category,
+
+			}).then(data => res.redirect("/products/" + data.id))
+
+			.catch(error => console.log(error))
+
+
 	},
 
 	// Update - Form to edit
 	edit: (req, res) => {
 		let editMarcas = db.Brands.findAll();
-		let product= db.Products.findByPk(req.params.id, {
-			include : ['category']
+		let product = db.Products.findByPk(req.params.id, {
+			include: ['category']
 		});
-		Promise.all([editMarcas,product])
-		.then(function([editMarcas,product]){
-				res.render("product-edit-form",{editMarcas,product})
-		
-		}).catch(function(err) {
-	  console.log(err)
-		})  
+		Promise.all([editMarcas, product])
+			.then(function ([editMarcas, product]) {
+					
+				res.render("product-edit-form", {
+					editMarcas,
+					product
+				})
+
+			}).catch(function (err) {
+				console.log(err)
+			})
 	},
 
-	
+
 	// Update - Method to update
 	update: (req, res) => {
-		db.Products.update({
-			title: req.body.title,
-			description: req.body.description,
-			photo: "/images/products/"+ req.files[0].filename,
-			price: req.body.price,
-			stock: req.body.stock,
-			
-			
-		  },{
-			where : {
-			  id : req.params.id 
-			}
-		  
-		 } ).then(data=>res.redirect("/products/"+req.params.id ))
+		
+					
 	
-		  .catch(error=>console.log(error))
+		db.Products.update({
+				title: req.body.title,
+				description: req.body.description,
+				photo: "/images/products/" + req.files[0].filename,
+				price: req.body.price,
+				stock: req.body.stock,
+
+			}, {
+				where: {
+					id: req.params.id
+				}
+				
+			}).then(data => res.redirect("/products/" + req.params.id))
+			
+			.catch(error => console.log(error))
 	},
 
 	// Delete - Delete one product from DB
-	destroy : (req, res) => {
-		// Do the magic
+	destroy: (req, res) => {
+
+		db.Products.destroy({
+				where: {
+					id: req.params.id
+				}
+
+			})
+			.then(data => res.redirect("/products"))
+
+			.catch(error => console.log(error))
+
 	}
+
+
 };
 
 module.exports = controller;
